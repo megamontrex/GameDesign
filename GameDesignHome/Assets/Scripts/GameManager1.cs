ba567f21bcd3a1fd9522896c101ec30f9d193cca
weaponmaster987 <34214878+weaponmaster987@users.noreply.github.com>
2017-12-16 23:13:47 +0100

    Game
    Added scoring and loadnextlvl

--- GameDesignHome/Assets/Scripts/GameManager1.cs
+++ GameDesignHome/Assets/Scripts/GameManager1.cs
@@ -2,15 +2,60 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager1 : MonoBehaviour {
+public class GameManager1 : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public static int PlayerScore1 = 0;
+    public static int PlayerScore2 = 0;
+
+    private LevelManager lm;
+
+    public GUISkin layout;
+
+    GameObject myBall;
+
+    // Use this for initialization
+    void Start()
+    {
+        PlayerScore1 = 0;
+        PlayerScore2 = 0;
+        myBall = GameObject.Find("Ball");
+        lm = GameObject.FindObjectOfType<LevelManager>();
+    }
+
+    public static void Score (string wallID)
+    {
+        if (wallID == "RightWall")
+        {
+            PlayerScore1++;
+        }
+        else
+        {
+            PlayerScore2++;
+        }
+    }
+    void OnGUI()
+    {
+        GUI.skin = layout;
+        GUI.Label(new Rect(Screen.width / 2 - 150 - 12, 20, 100, 100), "" + PlayerScore1);
+        GUI.Label(new Rect(Screen.width / 2 + 150 + 12, 20, 100, 100), "" + PlayerScore2);
+
+
+        if (PlayerScore1 == 3)
+        {
+            lm.LoadNextLevel();
+            
+        }
+        else if (PlayerScore2 == 3)
+        {
+            lm.LoadNextLevel();
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
---
+++ GameDesignHome/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        if(hitInfo.name == "Ball")
+        {
+            string wallName = transform.name;
+            GameManager1.Score(wallName);
+            hitInfo.gameObject.SendMessage("Restart Game", 1.0f, SendMessageOptions.RequireReceiver);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
