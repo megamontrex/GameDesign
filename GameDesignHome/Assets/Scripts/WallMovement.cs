dda1e330340d58dbcd57708d14350019ea90f94e
weaponmaster987 <34214878+weaponmaster987@users.noreply.github.com>
2017-12-17 18:24:05 +0100

    Game
    Final Submission

---
+++ GameDesignHome/Assets/Scripts/WallMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallMovement : MonoBehaviour {
+
+    public Transform Wall;
+    public float WallSpeed;
+	// Use this for initialization
+	void Start () {
+        Wall = GameObject.Find("Ball").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float move = WallSpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, Wall.position, move);
+	}
+}
